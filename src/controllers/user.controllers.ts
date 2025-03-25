59fe7ca2c688a73101110bd108dc2825db6494c7
{Your_Name} <Clint Tuyor>
2025-03-26 00:05:36 +0800

    .

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -58,18 +58,3 @@ export const updateUser = async (
   res.json(user);
   return;
 };
-
-// Delete user by ID
-export const deleteUser = async (
-  req: Request,
-  res: Response
-): Promise<void> => {
-  const user = await User.findByPk(req.params.id);
-  if (!user) {
-    res.status(404).json({ message: "User not found" });
-    return;
-  }
-  await user.destroy();
-  res.json({ message: "User deleted successfully" });
-  return;
-};
